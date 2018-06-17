703fed32731c78ff445f526061a1dc6f70132e8d
maxnovoguterres <35860401+maxnovoguterres@users.noreply.github.com>
2018-06-17 18:28:05 -0300

    pegando informações do txt

--- ProjectVikins/Assets/Script/DAL/MVC_Game2Context.cs
+++ ProjectVikins/Assets/Script/DAL/MVC_Game2Context.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using System.IO;
 
 namespace Assets.Script.DAL
 {
     public class MVC_Game2Context
     {
+        Dictionary<string, string> varTxt = new Dictionary<string, string>();
+        public MVC_Game2Context()
+        {
+            teste();
+        }
         //public PlayerPrefs Player = new PlayerPrefs();
         //PlayerPrefs a = new PlayerPrefs();
         public static List<CharacterType> CharactersType;
@@ -54,7 +60,27 @@ namespace Assets.Script.DAL
         }
 
         //PEGAR DADOS
+        public void teste()
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var dataDirectory = Path.Combine(currentDirectory, "teste");
+            var files = new DirectoryInfo(dataDirectory).GetFiles("*.txt");
+            foreach (var a in files)
+            {
+                StreamReader stwToLaw = new StreamReader(Path.Combine(dataDirectory, a.FullName));
+                //stwToLaw.ReadToEnd();
+                string b;
+                b = stwToLaw.ReadToEnd();
+                var c = b.Split(';');
+                foreach (var d in c)
+                {
+                    if (!d.Contains("="))
+                        continue;
+                    var e = d.Split('=');
+                    varTxt.Add(e[0], e[1]);
+                    Debug.Log(e[0] + " " + e[1]);
+                }
+            }
+        }
     }
-
-
 }
