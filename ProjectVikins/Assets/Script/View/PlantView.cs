5ec83fa59a9a3af55bfeb7c3af48e7c51010053e
bladek8 <lucas1999araujo@gmail.com>
2018-07-04 17:23:43 -0300

    checkpoint

---
+++ ProjectVikins/Assets/Script/View/PlantView.cs
@@ -0,0 +1,38 @@
+﻿using Assets.Script.Helpers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantView : MonoBehaviour
+{
+
+    private BoxCollider2D _boxCollider2D;
+    private BoxCollider2D BoxCollider2D { get { return _boxCollider2D ?? (_boxCollider2D = GetComponent<BoxCollider2D>()); } }
+
+    private Animator _animator;
+    private Animator Animator { get { return _animator ?? (_animator = GetComponent<Animator>()); } }
+    CountDown animatorCount;
+
+    private void Start()
+    {
+        animatorCount = new CountDown(0.5);
+    }
+
+    void Update()
+    {
+        CountDown.DecreaseTime(animatorCount);
+        if (animatorCount.ReturnedToZero)
+            Animator.SetBool("Collided", false);
+
+        transform.position = Utils.SetPositionZ(transform, BoxCollider2D.bounds.min.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.isTrigger)
+        {
+            Animator.SetBool("Collided", true);
+            animatorCount.StartToCount();
+        }
+    }
+}
