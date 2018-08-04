32d5a80bea988e1705883c9313eb340078b0d3c0
maxnovoguterres <35860401+maxnovoguterres@users.noreply.github.com>
2018-08-04 03:33:12 -0300

    inventario checkpoint

--- ProjectVikins/Assets/Script/View/InventoryView.cs
+++ ProjectVikins/Assets/Script/View/InventoryView.cs
@@ -20,6 +20,17 @@ namespace Assets.Script.View
         {
             if (Input.GetButtonDown("teste"))
             {
+                if (Inventary.activeSelf == true)
+                {
+                    var prefabs = Inventary.GetComponentsInChildren<Image>();
+
+                    foreach (var images in prefabs)
+                    {
+                        if (images.gameObject.name == "Icon_Coco(Clone)" || images.gameObject.name == "Icon_Coco_Aberto(Clone)")
+                        Destroy(images.gameObject);
+                    }
+                }
+
                 Inventary.SetActive(!Inventary.activeSelf);
                 Title.SetActive(!Title.activeSelf);
                 PrefabButtons = GetComponentsInChildren<Button>();
@@ -28,20 +39,22 @@ namespace Assets.Script.View
                 {
                     if (item.Amount >= 1 && item.ItemId == 1 && Inventary.activeSelf == true)
                     {
-                        var obj = Instantiate(item.Prefab, transform.position, Quaternion.identity, InventarySlot.transform);
-                        //var rectTrans = obj.GetComponent<RectTransform>();
+                        var obj = Instantiate(item.Prefab, new Vector3(0, 0, 0), Quaternion.identity, InventarySlot.transform);
+                        var rectTrans = obj.GetComponent<RectTransform>();
+                        rectTrans.anchoredPosition = new Vector3(0, 0, 0);
                         AmountText.enabled = true;
                         AmountText.text = item.Amount.ToString();
+                        obj.transform.SetSiblingIndex(0);
                         //var button = obj.GetComponent<Button>();
                         //button.onClick.AddListener(delegate { Interacted(item.InventoryItemId); });
                     }
                     else if (item.Amount >= 1 && item.ItemId == 2 && Inventary.activeSelf == true)
                     {
-                        var obj = Instantiate(item.Prefab, new Vector3(0, 0, 0), Quaternion.identity);
-                        obj.transform.SetParent(InventarySlot.transform);
-                        //var rectTrans = obj.GetComponent<RectTransform>();
-                        //rectTrans.anchoredPosition = new Vector3(0, 0, 0);
-                        //rectTrans.localScale = new Vector2(1, 1);
+                        var obj = Instantiate(item.Prefab, new Vector3(0, 0, 0), Quaternion.identity, InventarySlot.transform);
+                        //obj.transform.SetParent(transform);
+                        var rectTrans = obj.GetComponent<RectTransform>();
+                        rectTrans.anchoredPosition = new Vector3(0, 0, 0);
+                        obj.transform.SetSiblingIndex(0);
                         //var button = obj.GetComponent<Button>();
                         //button.onClick.AddListener(delegate { Interacted(item.InventoryItemId); });
                     }
@@ -49,14 +62,6 @@ namespace Assets.Script.View
                     {
                         AmountText.enabled = false;
                     }
-                }
-                if (Inventary.activeSelf == false)
-                {
-                    //PrefabButtons = GetComponentsInChildren<Button>();
-                    //foreach (var buttons in PrefabButtons)
-                    //{
-                    //    Destroy(buttons.gameObject);
-                    //}
                 }
             }
         }
